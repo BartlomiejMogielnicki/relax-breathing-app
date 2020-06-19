17f66cc4498bc073294741f8e5d004f93ff5cdb4
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-19 17:11:54 +0200

    Animations duration fully dependent of user settings. Values save to local storage.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container');
 const text = document.getElementById('text');
 
 const settingsContainer = document.getElementById('settings-container');
+const settingsForm = document.getElementById('settings-form');
 const settingsBtn = document.getElementById('settings-btn');
 const breathIn = document.getElementById('breath-in');
 const breathHold = document.getElementById('breath-hold');
@@ -9,27 +10,91 @@ const breathOut = document.getElementById('breath-out');
 const buttonSubmit = document.getElementById('submit-btn');
 const buttonClose = document.getElementById('close-btn');
 
-const totalTime = 7500;
-const resizeTime = (totalTime * 40) / 100;
-const holdTime = (totalTime * 20) / 100;
+const dot = document.getElementById('dot-container');
+
+let breathInTime, breathHoldTime, breathOutTime;
+
+// Show values in settings
+const updateSettingsValues = () => {
+    breathIn.value = breathInTime;
+    breathHold.value = breathHoldTime;
+    breathOut.value = breathOutTime;
+}
+
+// Get local storage data
+const getLocalStorage = () => {
+    const data = JSON.parse(localStorage.getItem('breathTime'));
+    console.log(data);
+    if (data !== null) {
+        breathInTime = data[0];
+        breathHoldTime = data[1];
+        breathOutTime = data[2];
+    } else {
+        breathInTime = 3;
+        breathHoldTime = 1.5;
+        breathOutTime = 3;
+    }
+}
+
+getLocalStorage();
+updateSettingsValues();
+
+let totalTime = breathInTime + breathHoldTime + breathOutTime;
+
+// Update dot animation
+const updateDotAnimation = () => {
+    dot.style.animationDuration = `${totalTime}s`;
+}
+
+updateDotAnimation();
+
+// Update circle gradient
+const updateUI = () => {
+
+}
 
 const breathFunction = () => {
+    updateUI();
     text.textContent = 'Breath in';
     container.className = 'container grow';
+    const containerGrow = document.querySelector('.container.grow').style.animationDuration = `${breathInTime}s`;
     setTimeout(() => {
         text.textContent = 'Hold';
         setTimeout(() => {
             text.textContent = 'Breath out';
             container.className = 'container shrink';
-        }, holdTime);
-    }, resizeTime);
+            const containerShrink = document.querySelector('.container.shrink').style.animationDuration = `${breathOutTime}s`
+        }, (breathHoldTime * 1000));
+    }, (breathOutTime * 1000));
 };
+
+setInterval(breathFunction, (totalTime * 1000));
 
 breathFunction();
 
-setInterval(breathFunction, totalTime);
+// Set local storage
+const setLocalStorage = () => {
+    let timesArr = [breathInTime, breathHoldTime, breathOutTime];
+    localStorage.setItem('breathTime', JSON.stringify(timesArr));
+}
+
+// Submit form and update time values
+const updateTimeValues = () => {
+    breathInTime = parseInt(breathIn.value);
+    breathHoldTime = parseInt(breathHold.value);
+    breathOutTime = parseInt(breathOut.value);
+    totalTime = breathInTime + breathHoldTime + breathOutTime;
+    setLocalStorage();
+}
 
 // Show/hide settings
 settingsBtn.addEventListener('click', () => {
     settingsContainer.classList.toggle('show');
+});
+
+// Submit settings form
+settingsForm.addEventListener('submit', (e) => {
+    updateTimeValues();
+    updateDotAnimation();
+    breathFunction();
 })
