c2e433c8a7f6e5d7248ff44766ffb61af1e7eb6c
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-19 14:40:11 +0200

    Show/hide settings container

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
+const settingsContainer = document.getElementById('settings-container');
+const settingsBtn = document.getElementById('settings-btn');
+const breathIn = document.getElementById('breath-in');
+const breathHold = document.getElementById('breath-hold');
+const breathOut = document.getElementById('breath-out');
+const buttonSubmit = document.getElementById('submit-btn');
+const buttonClose = document.getElementById('close-btn');
+
 const totalTime = 7500;
 const resizeTime = (totalTime * 40) / 100;
 const holdTime = (totalTime * 20) / 100;
@@ -20,3 +28,8 @@ const breathFunction = () => {
 breathFunction();
 
 setInterval(breathFunction, totalTime);
+
+// Show/hide settings
+settingsBtn.addEventListener('click', () => {
+    settingsContainer.classList.toggle('show');
+})
